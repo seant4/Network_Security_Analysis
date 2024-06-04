3eb9dce44614d37c46a69f232d2e56a23ba9590d
Sean Theisen <seanjtheisen@gmail.com>
2024-06-04 16:02:44 -0400

    Updates

--- alert.py
+++ alert.py
@@ -1,27 +1,38 @@
 from scapy.all import *
 from pcap_handler import *
 import pandas as pd
+import threading
 
 print("Capturing packets")
-capture = sniff(count=10)
-source = []
-destination = []
-sport = []
-dport = []
+flag = False
 
-for i in capture:
-    if IP in i:
-        if IPv6 not in i:
-            source.append(i[IP].src)
-            destination.append(i[IP].dst)
-            sport.append(i[TCP].sport)
-            dport.append(i[TCP].dport)
+def interrupt():
+    global flag
+    input("Press any key to stop\n")
+    flag = True
 
+def main_capture():
+    source = []
+    destination = []
+    sport = []
+    dport = []
+    interrupt_thread = threading.Thread(target=interrupt)
+    interrupt_thread.start()
+    while(flag == False):
+        capture = sniff(count=10)
 
-data = {'IP': destination, 'Port': dport}
-df = pd.DataFrame(data)
-fg = pd.pivot_table(df, index='Port', columns='IP', aggfunc='size', fill_value=0)
-print(fg)
-rs = fg.sum(axis=1).tolist()
-print(rs)
+        for i in capture:
+            if (IP in i) and (TCP in i):
+                if IPv6 not in i:
+                    source.append(i[IP].src)
+                    destination.append(i[IP].dst)
+                    sport.append(i[TCP].sport)
+                    dport.append(i[TCP].dport)
+        data = {'IP': destination, 'Port': dport}
+        df = pd.DataFrame(data)
+        fg = pd.pivot_table(df, index='Port', columns='IP', aggfunc='size', fill_value=0)
+        print(fg)
+        rs = fg.sum(axis=1).tolist()
+        print(rs)
 
+main_capture()
