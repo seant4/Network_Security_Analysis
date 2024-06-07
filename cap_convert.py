1ee17ce321aebd7426dd9868c8c19f711cb617ea
Sean Theisen <seanjtheisen@gmail.com>
2024-06-06 22:46:58 -0400

    Fin

---
+++ cap_convert.py
@@ -0,0 +1,28 @@
+from scapy.all import *
+from pcap_handler import *
+import pandas as pd
+import threading
+import os
+import time
+
+print("Capturing packets")
+
+def convert_capture(capture):
+    source = []
+    destination = []
+    sport = []
+    dport = []
+    for i in capture:
+        if (IP in i) and (TCP in i):
+            if IPv6 not in i:
+                source.append(i[IP].src)
+                destination.append(i[IP].dst)
+                sport.append(i[TCP].sport)
+                dport.append(i[TCP].dport)
+    data = {'Source': source, 'Destination': destination, "Source Port": sport, "Destination Port": dport}
+    df = pd.DataFrame(data)
+    df.to_csv("table.csv")
+
+#cap = rdpcap("") #Pull from file
+cap = sniff(count=10)
+convert_capture(cap)
--- packet_analysis.R
+++ packet_analysis.R
@@ -1,1 +1,13 @@
-out <- read.csv("C:/Users/seanj/projects/ai_security/out.csv")
+import(ggplot2)
+
+table<- read.csv("path")
+table <- table[,-1]
+destination <- data.frame(table[,2], table[,4])
+names(destination) <- c("Destination IP", "Destination Port")
+dtable <- as.data.frame(table(destination))
+source <- data.frame(table[,1], table[,3])
+names(source) <- c("Source IP", "Source Port")
+stable <- as.data.frame(table(source))
+ggplot(dtable, aes(Destination.IP, Destination.Port, fill=Freq)) + geom_tile()
+ggplot(stable, aes(Source.IP, Source.Port, fill=Freq)) + geom_tile()
+table(destination)
